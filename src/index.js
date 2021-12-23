36d33f8fb0ad778a731733cd6b74d901c3fa6af4
Oleg Kireev <okireev@ubic.tech>
2021-12-23 17:46:47 +0300

    @/index: create express app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+
+const app = express();
+const port = process.env.port || 4000;
+
+app.get('/', (req, res) => res.send('Hello Express'));
+app.listen(port, () => console.log(`Server has been started at ${port} http://localhost:${port}`))
