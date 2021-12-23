fd47ca524b2666d762cc4a6717a5e88a6bb20c82
Oleg Kireev <okireev@ubic.tech>
2021-12-23 17:47:44 +0300

    @/index: create Apollo GraphQL server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
+// Express App
 const app = express();
 const port = process.env.port || 4000;
 
+// Scheme
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => `Hello GraphQL`
+  }
+};
+
+// Apollo GraphQL server
+const server = new ApolloServer({ typeDefs, resolvers});
+server.applyMiddleware({ app, path: '/api'})
+
+
 app.get('/', (req, res) => res.send('Hello Express'));
-app.listen(port, () => console.log(`Server has been started at ${port} http://localhost:${port}`))
+app.listen(port, () => console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`))
