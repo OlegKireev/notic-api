7b8da76e36702597fb994f0efbefa3670c9b004e
Oleg Kireev <okireev@ubic.tech>
2022-01-10 20:08:47 +0300

    @/index: create mock notes query

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+const mockNotesData = require('./mock/notes');
 
 // Express App
 const app = express();
@@ -7,20 +8,26 @@ const port = process.env.port || 4000;
 
 // Scheme
 const typeDefs = gql`
+  type Note {
+    id: ID!,
+    content: String!,
+    author: String!,
+    date: String!,
+  }
   type Query {
-    hello: String
+    notes: [Note!]!,
   }
 `;
+
 const resolvers = {
   Query: {
-    hello: () => `Hello GraphQL`
-  }
+    notes: () => mockNotesData,
+  },
 };
 
 // Apollo GraphQL server
 const server = new ApolloServer({ typeDefs, resolvers});
 server.applyMiddleware({ app, path: '/api'})
-
 
 app.get('/', (req, res) => res.send('Hello Express'));
 app.listen(port, () => console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`))
