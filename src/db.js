cfbc6b90d69eed6ef2931c201cd30adb3dddc3c5
Oleg Kireev <okireev@ubic.tech>
2022-01-10 21:25:58 +0300

    @/db: create mongoose db config

---
+++ src/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+module.exports = {
+  connect: (DB_HOST) => {
+    mongoose.set('useNewUrlParser', true);
+    mongoose.set('useFindAndModify', false);
+    mongoose.set('useCreateIndex', true);
+    mongoose.set('useUnifiedTopology', true);
+
+    mongoose.connect(DB_HOST);
+
+    mongoose.connection.on('error', (err) => {
+      console.err(err);
+      console.log(
+        'MongoDB connection error. Please make sure MongoDB is running.'
+      );
+      process.exit();
+    });
+  },
+  close: () => {
+    mongoose.connection.close();
+  }
+}
