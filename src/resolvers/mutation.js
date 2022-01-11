ac5ca2b18362b6dc74da247680ee8e428e7c538a
Oleg Kireev <okireev@ubic.tech>
2022-01-11 13:22:35 +0300

    @/resolvers/mutation: create signUp mutation

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,3 +1,14 @@
+// Импорт пакета для шифрования паролей
+const bcrypt = require('bcrypt');
+// Импорт пакета для генерации json web token
+const jwt = require('jsonwebtoken');
+// Подключение переменных окружения .env
+require('dotenv').config();
+
+// Импорт функции-генератора ссылки для аватара пользователя
+const gravatar = require('../util/gravatar');
+
+
 module.exports = {
   createNote: async (parent, args, { models }) => {
     return await models.Note.create({
@@ -23,5 +34,26 @@ module.exports = {
       },
       {new: true},
     );
+  },
+  signUp: async (parent, { username, email, password }, { models }) => {
+    // Нормализация email
+    email = email.trim().toLowerCase();
+    // Хеширование пароля
+    const hashed = await bcrypt.hash(password, 10);
+    // Создаем url аватара
+    const avatar = gravatar(email);
+    try {
+      // Создаем объект нового пользователя
+      const user = await models.User.create({
+        username,
+        email,
+        password: hashed,
+        avatar,
+      });
+      return jwt.sign({id: user._id}, process.env.JWT_SECRET);
+    } catch (err) {
+      console.log(err);
+      throw new Error('Error creating account');
+    }
   }
 };
